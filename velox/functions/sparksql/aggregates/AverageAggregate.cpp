3e397f306a410710420fac5e27cc6fb0586ec76e
joey.ljy <joey.ljy@alibaba-inc.com>
2023-12-23 20:10:48 +0800

    Revert "Fix decimal agg signature on partial companion function (#465)"
    This reverts commit a4899ec54b92acb09fa9392b4a150f4115a88ad3.

--- velox/functions/sparksql/aggregates/AverageAggregate.cpp
+++ velox/functions/sparksql/aggregates/AverageAggregate.cpp
@@ -429,9 +429,7 @@ exec::AggregateRegistrationResult registerAverage(
                 auto inputScale = inputType->asShortDecimal().scale();
                 auto sumType =
                     DECIMAL(std::min(38, inputPrecision + 10), inputScale);
-                if (exec::isPartialOutput(step) ||
-                    (step == core::AggregationNode::Step::kSingle &&
-                     resultType->isRow())) {
+                if (exec::isPartialOutput(step)) {
                   return std::make_unique<
                       DecimalAverageAggregate<int64_t, int64_t>>(
                       resultType, sumType);
